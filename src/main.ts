8d81cb1024fd2907589f9c6f9a7cda2a284cc002
Aleksandr Dederer <171031023+Dederer-A@users.noreply.github.com>
2026-07-24 14:55:09 +0530

    chore: add support i18n localization

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+
+import i18n from './locales/i18n'; // Import the i18n configuration
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
