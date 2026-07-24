71b0359b1098f32a3f8dccade2233af16f0945f0
Aleksandr Dederer <171031023+Dederer-A@users.noreply.github.com>
2026-07-24 22:56:31 +0530

    fix(config): remove wasm from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import wasm from 'vite-plugin-wasm';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), wasm()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
