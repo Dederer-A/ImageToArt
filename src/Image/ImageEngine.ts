edd73316feeb5b2a2be70f5678747ae703083e31
Aleksandr Dederer <171031023+Dederer-A@users.noreply.github.com>
2026-08-03 05:35:58 +0530

    chore: remove log for better performance

--- src/Image/ImageEngine.ts
+++ src/Image/ImageEngine.ts
@@ -18,9 +18,9 @@ const debouncedDraw = debounce((documentRuntime: any) => {
     // TODO change if logic. If something goes wrong stop processing
     if (layer.enabled && layerEngine != null) {
       // console.log(`[ImageEngine] render: ${layer.type} | ${JSON.stringify(layer.properties)}`);
-      console.time(layer.type);
+      // console.time(layer.type);
       currentImageData = layerEngine.render(documentRuntime, currentImageData, layer.properties);
-      console.timeEnd(layer.type);
+      // console.timeEnd(layer.type);
     }
   }
   console.timeEnd('ImageEngine');
