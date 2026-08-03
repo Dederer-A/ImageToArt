b290d5878ee436479b12f46b6cef19d8e1b7e317
Aleksandr Dederer <171031023+Dederer-A@users.noreply.github.com>
2026-08-03 05:31:47 +0530

    chore: remove cache for ImageData

--- src/workplace/runtime.ts
+++ src/workplace/runtime.ts
@@ -3,21 +3,18 @@ import { ImageEngine } from '@/Image/ImageEngine';
 export class VariantRuntime {
   readonly variantId: string;
   renderedImageData: ImageData | null = null;
-  layerCache: Map<string, ImageData> = new Map();
 
   constructor(variantId: string) {
     this.variantId = variantId;
   }
 
   public invalidate() {
-    this.layerCache.clear();
     this.renderedImageData = null;
   }
 
   public clone(variantId: string): VariantRuntime {
     const newRuntime = new VariantRuntime(variantId);
     newRuntime.renderedImageData = ImageEngine.cloneImageData(this.renderedImageData);
-    newRuntime.layerCache = new Map();
     return newRuntime;
   }
 }
